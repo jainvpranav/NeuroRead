e8da6d7ae89cf858cf29314cfc0a99fec6239fda
Pranav <pranavv2002@gmail.com>
2025-04-24 23:41:31 +0530

    Community Page - In Progress (Error in handleLike function)

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -28,3 +28,45 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
 }
+
+export async function POST(req: NextRequest) {
+  try {
+    const { type, data } = await req.json();
+    console.log(type, data);
+    if (type === "like") {
+      if (data.like) {
+        data.post_data.like += 1;
+        console.log(data.post_data.like);
+        const { data: result, error } = await supabase
+          .from("posts")
+          .update({ like: data.post_data.like })
+          .eq("post_id", data.post_data.post_id)
+          .select();
+        if (error) {
+          return NextResponse.json({ error: error }, { status: 501 });
+        }
+        if (result) {
+          return NextResponse.json({ message: "Yay 204" }, { status: 204 });
+        }
+      } else {
+        data.post_data.like -= 1;
+        console.log(data.post_data.like);
+        const { data: result, error } = await supabase
+          .from("posts")
+          .update({ like: data.post_data.like })
+          .eq("post_id", data.post_data.post_id)
+          .select();
+        if (error) {
+          NextResponse.json({ error: error }, { status: 501 });
+        }
+        if (result) {
+          return NextResponse.json({ message: "Yay 204" }, { status: 204 });
+        }
+      }
+    }
+    return NextResponse.json({ error: "Some Error" }, { status: 501 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
--- app/community/page.tsx
+++ app/community/page.tsx
@@ -13,55 +13,108 @@ import {
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Heart, Share2 } from "lucide-react";
 import axios from "axios";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Button } from "@/components/ui/button";
 
 export default function Community() {
   const router = useRouter();
   const [posts, setPosts] = useState([]);
+  const [postLike, setPostLike] = useState([false]);
   useEffect(() => {
     const getPosts = async () => {
       const posts = await axios.get("/api/posts");
-      console.log(posts);
-      if (posts.status === 200) setPosts(posts.data);
+      if (posts.status === 200) {
+        setPosts(posts.data);
+        setPostLike(
+          posts.data.map((post: any) => {
+            return false;
+          })
+        );
+      }
     };
     getPosts();
   }, []);
+
+  function handleLike(e: any, index: number): any {
+    e.preventDefault();
+    setPostLike((currentLikes: any) => {
+      const newLikes = [...currentLikes]; // Create a copy of the array
+      newLikes[index] = !currentLikes[index]; // Modify the copy
+      return newLikes; // Return the modified copy
+    });
+
+    setPosts((prevPosts: any) =>
+      prevPosts.map((post: any, i: number) =>
+        i === index
+          ? { ...post, like: post.like + (postLike[index] ? -1 : 1) }
+          : post
+      )
+    );
+
+    const like = axios.post("/api/posts", {
+      type: "like",
+      data: {
+        like: postLike[index],
+        post_data: {
+          post_id: posts[index]["post_id"],
+          like: posts[index]["likes"],
+        },
+      },
+    });
+  }
+
   return (
     <div className="flex min-h-screen flex-col">
       <Navbar />
       <main className="flex-1 container py-8 border-2 border-indigo-500 overflow-auto ">
         <div className="columns-2 md:columns-3 lg:columns-4 gap-4">
-          {posts.map((post: any, index: number) => {
-            return (
-              <Card
-                className="mb-4 break-inside-avoid p-2 rounded shadow"
-                key={index}
-              >
-                <CardHeader className="m-0 p-0">
-                  <div className="flex flex-row items-center gap-2">
-                    <Avatar className="m-1">
-                      <AvatarImage src={post.profile_pic_link} />
-                      <AvatarFallback>NR</AvatarFallback>
-                    </Avatar>
-                    <span className="text-xs">{post.username}</span>
-                  </div>
-                </CardHeader>
-                <CardContent>
-                  <div>
-                    <span>{post.description}</span>
-                  </div>
-                  <img
-                    className="rounded-md"
-                    src={post.image_link}
-                    alt="Community Post Image"
-                  />
-                </CardContent>
-                <CardFooter className="flex justify-center items-center gap-2">
-                  <Heart />
-                  <Share2 />
-                </CardFooter>
-              </Card>
-            );
-          })}
+          {posts.length >= 1 ? (
+            posts.map((post: any, index: number) => {
+              return (
+                <Card
+                  className="mb-4 break-inside-avoid p-2 rounded shadow"
+                  key={index}
+                >
+                  <CardHeader className="m-0 p-0">
+                    <div className="flex flex-row items-center gap-2">
+                      <Avatar className="m-1">
+                        <AvatarImage src={post.profile_pic_link} />
+                        <AvatarFallback>NR</AvatarFallback>
+                      </Avatar>
+                      <span className="text-xs">{post.username}</span>
+                    </div>
+                  </CardHeader>
+                  <CardContent>
+                    <div>
+                      <span>{post.description}</span>
+                    </div>
+                    <img
+                      className="rounded-md"
+                      src={post.image_link}
+                      alt="Community Post Image"
+                    />
+                  </CardContent>
+                  <CardFooter className="flex justify-center items-center gap-2">
+                    <Button
+                      className="bg-transparent hover:bg-transparent"
+                      onClick={() => {
+                        handleLike(event, index);
+                      }}
+                    >
+                      {postLike[index] && <Heart color="red" />}
+                      {!postLike[index] && <Heart color="white" />}
+                      <span className="text-white">{post.likes}</span>
+                    </Button>
+                    <Button className="bg-transparent hover:bg-transparent">
+                      <Share2 color="white" />
+                    </Button>
+                  </CardFooter>
+                </Card>
+              );
+            })
+          ) : (
+            <Skeleton className="w-100 h-100 rounded-xl" />
+          )}
         </div>
       </main>
     </div>
